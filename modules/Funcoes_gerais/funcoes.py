8e2eb15f2168a7561f27117df9aea9cc0ed8796c
AdrianoCarvalh0 <adriano.carvalho@ifsuldeminas.edu.br>
2023-07-20 12:57:43 -0300

    consertando o algoritimo que expande as imagens

--- modules/Funcoes_gerais/funcoes.py
+++ modules/Funcoes_gerais/funcoes.py
@@ -6,9 +6,9 @@ def ler_diretorios(dir):
     filenames = []
     for filename in os.listdir(dir):
     #     # Use only images having magnification 20x
-    #     #if 'Experiment #1 (adults set #1)_20x_batch1 - Superfical layers' in filename:
+        if 'Experiment #1 (adults set #1)_20x_batch1 - Superfical layers@40-Image 3-20X' in filename:
     #     if 'T-3 Weeks' in filename:      
-        filenames.append(filename.split('.')[0])
-    
+        #filenames.append(filename.split('.')[0])
+            filenames.append(filename)
     #filenames = filenames[:20]
     return filenames
