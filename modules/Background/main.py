cc7b1368fafbf795dbf8d512c2cdfe2ba563c554
AdrianoCarvalh0 <adriano.carvalho@ifsuldeminas.edu.br>
2023-07-21 13:15:47 -0300

    add

--- modules/Background/main.py
+++ modules/Background/main.py
@@ -1,48 +1,68 @@
-import pickle, os
-import tracemalloc, time
+import pickle
 import numpy as np
 from PIL import Image
 import sys
+import geopandas as gpd
+
 sys.path.insert(0, "/home/adriano/projeto_mestrado/modules")
+
+from Funcoes_gerais import funcoes
 
 import background_generation as backgen
 
-root_dir = "/home/adriano/projeto_mestrado/modules"
+root_dir = f"/home/adriano/projeto_mestrado/modules"
 
-img_dir = root_dir + '/Imagens/vessel_data/images/'
-lab_dir = root_dir + '/Imagens/vessel_data/labels_20x/'
-
-#Varrendo o diretório
-filenames = []
-for filename in os.listdir(img_dir):
-#     # Use only images having magnification 20x
-#     #if 'Experiment #1 (adults set #1)_20x_batch1 - Superfical layers' in filename:
-#     if 'T-3 Weeks' in filename:      
-  filenames.append(filename.split('.')[0])
-filenames = filenames[:20]
-
-#Lendo o pickle e gerando o vessel_map
-idx = 1
+img_dir = f'{root_dir}/Imagens/vessel_data/images/'
+lab_dir = f'{root_dir}/Imagens/vessel_data/labels_20x/'
 pickle_dir = f'{root_dir}/Vessel_Models_pickle'
-path = (pickle_dir + f'{filenames[idx]}_savedata1.pickle')
-arquivo = pickle.load(open(path, 'rb'))
-vessel_map = arquivo['vessel_model'].vessel_map
-mapa_original = vessel_map.mapped_values
 
-#Lendo o Json
-#arquivo = '/content/drive/MyDrive/Mestrado em Ciência da Computação/Artificial Lines/arquivo_quatro_pontos.json'
-arquivo = '{root_dir}/Artificial Lines/teste_31_05.json'
-#arquivo = '/content/drive/MyDrive/Mestrado em Ciência da Computação/Artificial Lines/teste2_31_05.json'
-#arquivo = '/content/drive/MyDrive/Mestrado em Ciência da Computação/Artificial Lines/teste.json'
+img_especifica = 'Experiment #1 (adults set #1)_20x_batch1 - Superfical layers@40-Image 3-20X'
+array_pickles = funcoes.ler_diretorios(pickle_dir, img_especifica)
+vetor_informacoes = []
 
-medial_path = backgen.retorna_paths(arquivo)
+for i in range(len(array_pickles)):
 
-caminhos_transladados = backgen.retorna_caminhos_transladados(medial_path)
-caminhos_transladados_interpolado = backgen.retorna_caminhos_transladados(medial_path[0], 2)
+  path = (pickle_dir + f'/{array_pickles[i]}')
+  arquivo_pickle = pickle.load(open(path, 'rb'))
+  vessel_map = arquivo_pickle['vessel_model'].vessel_map 
+  mapa_original = vessel_map.mapped_values,
+  imagem_binaria_original = vessel_map.mapped_mask_values
+  imagem_binaria_sem_artefatos_laterais = backgen.retornar_imagem_binaria_sem_artefatos(vessel_map, imagem_binaria_original)
+  imagem_binaria_sem_artefatos = backgen.fill_holes(imagem_binaria_sem_artefatos_laterais)
+  rows, cols = mapa_original[0].shape
+  distancia = (rows/2)
+ 
+  dict = {
+      "path": path,
+      "arquivo_pickle": arquivo_pickle,
+      "vessel_map": vessel_map,
+      "mapa_original": mapa_original,
+      "imagem_binaria_original" : imagem_binaria_original,
+      "imagem_binaria_sem_artefatos_laterais" : imagem_binaria_sem_artefatos_laterais,  
+      "imagem_binaria_sem_artefatos" : imagem_binaria_sem_artefatos,    
+      "rows": rows,
+      "cols": cols,
+      "distancia": distancia
+    }
+  vetor_informacoes.append(dict)
 
-vetor_pontos = backgen.construindo_caminhos(caminhos_transladados[0])
-vetor_pontos_interp = backgen.construindo_caminhos(caminhos_transladados_interpolado[0])
+array_tracados = f'{root_dir}/Artificial_Lines/array_arquivos.json'
 
-linha_central,linha_offset_esquerda,linha_offset_direita, maior_tamanho = backgen.retorna_linhas_offset_posicao_tamanho(mapa_original,caminhos_transladados_interpolado[0])
+medial_path = backgen.retorna_paths(array_tracados)
 
-dst_array_np = backgen.retorna_dst_array_np(linha_central,linha_offset_esquerda,linha_offset_direita, maior_tamanho)
+for i in range(len(medial_path)):    
+    caminhos_transladados_interpolado = backgen.retorna_caminhos_transladados(medial_path[i], 2) # type: ignore
+    distancia = vetor_informacoes[i]['distancia'] # type: ignore
+    print(distancia)
+    linha_central,linha_offset_esquerda,linha_offset_direita, maior_tamanho = backgen.retorna_linhas_offset_posicao_tamanho(caminhos_transladados_interpolado[0],distancia)
+
+    gp4 = gpd.GeoSeries([linha_offset_esquerda, linha_central, linha_offset_direita])
+    gp4.plot(cmap="tab10")
+
+
+
+
+
+
+
+
--- modules/Funcoes_gerais/funcoes.py
+++ modules/Funcoes_gerais/funcoes.py
@@ -2,13 +2,13 @@ import os
 import sys
 sys.path.insert(0, "/home/adriano/projeto_mestrado/modules")
 
-def ler_diretorios(dir):
+def ler_diretorios(dir, img=None):
     filenames = []
     for filename in os.listdir(dir):
-    #     # Use only images having magnification 20x
-        if 'Experiment #1 (adults set #1)_20x_batch1 - Superfical layers@40-Image 3-20X' in filename:
-    #     if 'T-3 Weeks' in filename:      
-        #filenames.append(filename.split('.')[0])
+        if img is not None:
+            if img in filename:   
+                filenames.append(filename)
+        else:
             filenames.append(filename)
-    #filenames = filenames[:20]
+    
     return filenames
