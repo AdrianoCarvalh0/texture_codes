e55d93b6c0f419423b4c0c7304da3cb8f24d29e1
AdrianoCarvalh0 <adriano.carvalho@ifsuldeminas.edu.br>
2023-08-06 18:30:13 -0300

    add dia 06-08

--- modules/Funcoes_gerais/funcoes.py
+++ modules/Funcoes_gerais/funcoes.py
@@ -1,5 +1,7 @@
 import os, sys, json
 import sys
+import time
+import tracemalloc
 sys.path.insert(0, "/home/adriano/projeto_mestrado/modules")
 
 def ler_diretorios(dir, img=None):
@@ -16,3 +18,14 @@ def ler_diretorios(dir, img=None):
 def gravar_array_arquivo(array_list, filename):  
   lista2 = [item.tolist() for item in array_list]
   json.dump(lista2, open(filename, 'w'), indent=2)
+
+
+def calcular_tempo_memoria(str, funcao):
+    tracemalloc.start()
+    start_time = time.time()          
+    end_time = time.time()
+    _, peak_memory = tracemalloc.get_traced_memory()
+    execution_time = end_time - start_time
+
+    print(f"Criação da {str} {execution_time} seconds, and the peak memory usage was {peak_memory/1024**3} GBs.")
+    tracemalloc.stop()  
