1de5bc4ecc78ca82a4ce398c5050fcbde1aca689
Adriano Carvalho <adriano.carvalho@ifsuldeminas.edu.br>
2023-12-06 18:38:29 -0300

    add dia 06/12

---
+++ modules/Background/exibir_backgrounds.py
@@ -0,0 +1,41 @@
+from matplotlib import pyplot as plt
+from pathlib import Path
+import sys
+import numpy as np
+from PIL import Image
+
+back1 = 'Experiment #1 (adults set #1)_20x_batch1 - Superfical layers@40-Image 1-20X.tiff'
+back2 = 'Experiment #1 (adults set #1)_20x_batch1 - Superfical layers@41-Image 4-20X.tiff'
+back3 = 'Experiment #1 (adults set #1)_20x_batch1 - Superfical layers@46-Image 2-20X.tiff'
+back4 = 'Experiment #1 (adults set #1)_20x_batch1 - Superfical layers@59-Image 4-20X.tiff'
+
+
+root_dir = Path(r"C:\Users\adria\Documents\Mestrado\texture_codes\modules")
+
+back_artif1 = np.array(Image.open(f'{root_dir}/Background/Mapas_gerados_artificialmente/{back1}'))
+back_artif2 = np.array(Image.open(f'{root_dir}/Background/Mapas_gerados_artificialmente/{back2}'))
+back_artif3 = np.array(Image.open(f'{root_dir}/Background/Mapas_gerados_artificialmente/{back3}'))
+back_artif4 = np.array(Image.open(f'{root_dir}/Background/Mapas_gerados_artificialmente/{back4}'))
+
+
+plt.figure(figsize=[15, 15])
+plt.imshow(back_artif1, 'gray', vmin=0, vmax=60)
+plt.savefig('back_artif1.tiff')
+plt.plot()
+
+plt.figure(figsize=[15, 15])
+plt.imshow(back_artif2, 'gray', vmin=0, vmax=60)
+plt.savefig('back_artif2.tiff')
+plt.plot()
+
+plt.figure(figsize=[15, 15])
+plt.imshow(back_artif3, 'gray', vmin=0, vmax=60)
+plt.savefig('back_artif3.tiff')
+plt.plot()
+
+plt.figure(figsize=[15, 15])
+plt.imshow(back_artif4, 'gray', vmin=0, vmax=60)
+plt.savefig('back_artif4.tiff')
+plt.plot()
+
+
