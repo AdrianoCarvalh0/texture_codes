8b0515ac1e103c8a67d7823fba561f9ca67f62b6
Adriano Carvalho <adriano.carvalho@ifsuldeminas.edu.br>
2024-01-10 11:15:16 -0300

    add dia 10/01

--- modules/Background/exibir_backgrounds.py
+++ modules/Background/exibir_backgrounds.py
@@ -9,8 +9,11 @@ back2 = 'Experiment #1 (adults set #1)_20x_batch1 - Superfical layers@41-Image 4
 back3 = 'Experiment #1 (adults set #1)_20x_batch1 - Superfical layers@46-Image 2-20X.tiff'
 back4 = 'Experiment #1 (adults set #1)_20x_batch1 - Superfical layers@59-Image 4-20X.tiff'
 
+#windows
+#root_dir = Path(r"C:\Users\adria\Documents\Mestrado\texture_codes\modules")
 
-root_dir = Path(r"C:\Users\adria\Documents\Mestrado\texture_codes\modules")
+#linux
+root_dir = f"/home/adriano/projeto_mestrado/modules"
 
 back_artif1 = np.array(Image.open(f'{root_dir}/Background/Mapas_gerados_artificialmente/{back1}'))
 back_artif2 = np.array(Image.open(f'{root_dir}/Background/Mapas_gerados_artificialmente/{back2}'))
@@ -20,22 +23,22 @@ back_artif4 = np.array(Image.open(f'{root_dir}/Background/Mapas_gerados_artifici
 
 plt.figure(figsize=[10, 10])
 plt.imshow(back_artif1, 'gray', vmin=0, vmax=60)
-plt.savefig('back_artif1.tiff')
+plt.savefig('back_artif1.svg', format='svg')
 plt.plot()
 
 plt.figure(figsize=[10, 10])
 plt.imshow(back_artif2, 'gray', vmin=0, vmax=60)
-plt.savefig('back_artif2.tiff')
+plt.savefig('back_artif2.svg', format='svg')
 plt.plot()
 
 plt.figure(figsize=[10, 10])
 plt.imshow(back_artif3, 'gray', vmin=0, vmax=60)
-plt.savefig('back_artif3.tiff')
+plt.savefig('back_artif3.svg', format='svg')
 plt.plot()
 
 plt.figure(figsize=[10, 10])
 plt.imshow(back_artif4, 'gray', vmin=0, vmax=60)
-plt.savefig('back_artif4.tiff')
+plt.savefig('back_artif4.svg', format='svg')
 plt.plot()
 
 
