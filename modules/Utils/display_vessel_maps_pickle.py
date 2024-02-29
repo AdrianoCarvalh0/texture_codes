e777a5cd9fb5c21e8316dd58a5cecb8f1667362c
Adriano dos Reis Carvalho <adrianocarvalho@CPD12L.muzambinho.edu.br>
2024-02-29 18:54:21 -0300

    add dia 29-02

--- modules/Utils/display_vessel_maps_pickle.py
+++ modules/Utils/display_vessel_maps_pickle.py
@@ -3,12 +3,12 @@ from matplotlib import pyplot as plt
 from pathlib import Path
 
 # Linux
-#sys.path.insert(0, "/home/adriano/projeto_mestrado/modules")
-#root_dir = f"/home/adriano/projeto_mestrado/modules"
+sys.path.insert(0, "/home/adriano/projeto_mestrado/modules")
+root_dir = f"/home/adriano/projeto_mestrado/modules"
 
 # Windows
-sys.path.insert(0, r"C:\Users\adria\Documents\Mestrado\texture_codes\modules")
-root_dir = Path(r"C:\Users\adria\Documents\Mestrado\texture_codes\modules")
+#sys.path.insert(0, r"C:\Users\adria\Documents\Mestrado\texture_codes\modules")
+#root_dir = Path(r"C:\Users\adria\Documents\Mestrado\texture_codes\modules")
 
 from Utils import functions
 
@@ -26,6 +26,8 @@ for i in range(len(vector_pickles)):
     original_map = vessel_map.mapped_values
     print(f'/{vector_pickles[i]}')
     plt.figure(figsize=[10, 8])
-    plt.title("original_map")
+    #plt.title("original_map")
     plt.imshow(original_map, 'gray', vmin=0, vmax=60)
     plt.plot()
+    plt.axis('off')
+    plt.savefig(f'img_{i}.svg', format='svg')
